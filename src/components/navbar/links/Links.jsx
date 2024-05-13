6a3434152c37a88d1d12aa85757dbfabf3248216
yusuf <yusuffdemirci10@gmail.com>
2024-05-13 14:59:47 +0300

    responsive menu created

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
   const links = [
     {
       title: "Ana sayfa",
@@ -27,17 +29,34 @@ const Links = () => {
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Yönetici", path: "/admin" }} />}
-          <button className={styles.logout}>Çıkış</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Giriş", path: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && (
+              <NavLink item={{ title: "Yönetici", path: "/admin" }} />
+            )}
+            <button className={styles.logout}>Çıkış</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Giriş", path: "/login" }} />
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menü
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
