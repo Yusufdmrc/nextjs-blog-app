29983ce4b073b34a2e1df37a7766cf4537a4a525
yusuf <yusuffdemirci10@gmail.com>
2024-05-15 14:44:47 +0300

    contact page was designed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="İsim ve Soyisim" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Telefon numarası(opsiyonel)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Mesaj"
+          ></textarea>
+          <button>Gönder</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
